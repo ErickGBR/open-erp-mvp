def138a4d0e09f7f2c6c3d0b5cadca5e068b853a
Erick Burgos <eburgosrivas1997@gmail.com>
2026-07-24 01:14:47 -0600

    fix: import ROLES_KEY from roles.decorator instead of redefining

--- backend/src/auth/roles.guard.ts
+++ backend/src/auth/roles.guard.ts
@@ -1,8 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { UserRole } from '../users/user.entity';
-
-export const ROLES_KEY = 'roles';
+import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
